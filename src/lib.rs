6b672bd3541b42f104daef95d0ed7da46189804e
jysndabuio <jysndabu@outlook.com>
2024-03-19 22:19:48 +0100

    My solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
-    )
+      let mut counter: Option<u64> = Some(0);
+
+    while n != 1 {
+        if n % 2 == 0 {
+            // n = n / 2;
+            let _n = n / 2;
+            // counter += 1; Not valid for Option type
+            counter = counter.map(|c| c + 1);
+        } else {
+            // n = (3 * n ) + 1;
+            let _n = (3 * n ) + 1;
+            // counter += 1;
+            counter = counter.map(|c| c + 1);
+        }
+    }
+    counter
 }
